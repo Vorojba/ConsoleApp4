3e33a711803e5705765e6e512bb8ea340a89313d
Tester <test7@user.com>
2021-10-19 11:41:29 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;//объявление переменной
+            Console.Write("Введите x = ");//Принлашение к вводу
+            x = Convert.ToInt32(Console.ReadLine());//Присваивание значений
+            if (x % 4 == 0)
+                Console.WriteLine("y=" + x / 4);
+
+            else
+                Console.WriteLine("Введеное число не делится нацело");
+            Console.ReadKey();
+        }
+    }
+}
